dc9f6479669f530a84cbc844e26ec43cd983d859
Sujeet Gund <sujeetgund@gmail.com>
2022-07-09 10:52:11 +0530

    favicon changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   )
